20c244af77834e07dd71f3b75c10d1d1ef62fda7
Viacheslav Wolf <vwo@netlands.de>
2013-02-03 22:49:39 +0100

    Icons werden in Notifications angezeigt

--- home/skriptai/creative/remote/switch_control.sh
+++ home/skriptai/creative/remote/switch_control.sh
@@ -7,7 +7,7 @@ SET_NEXT_CONTROL=false
 FIRST_CONTROL_SETTED=false
 FIRST_CONTROL=""
 
-for i in `ls -B --ignore *_desc $CONTROL_PATH/`
+for i in `ls -B --ignore *_desc --ignore *.png $CONTROL_PATH/`
 do
 	if [ $FIRST_CONTROL_SETTED != true ]
 		then
@@ -31,13 +31,15 @@ fi
 
 TITLE="IR-Control"
 DESC="IR-Control mode switched to use $NEXT_CONTROL"
+ICON="../remote_control.png"
 DESCR_FILE=$CONTROL_PATH/$NEXT_CONTROL"_desc"
 
 test -f $DESCR_FILE && source $DESCR_FILE
+test -f "$CONTROL_PATH/$NEXT_CONTROL.png" && ICON="$NEXT_CONTROL.png"
 
 echo $NEXT_CONTROL
 echo $NEXT_CONTROL > $CONTROL_PATH/../loadet_control
-notify-send -t 2000 "$TITLE" "$DESC"
+notify-send -t 2000 -i "$CONTROL_PATH/$ICON" "$TITLE" "$DESC"
 
 killall irexec
 echo include $CONTROL_PATH"/../def_lircrc" > $CONTROL_PATH"/../lircrc"
